825273c44e84f54db6c9c7904c7733d3fe276a64
s22017 <itc-s22017@std.it-college.ac.jp>
2024-12-05 13:57:44 +0900

    hmhmhm

--- app/utils/socket.tsx
+++ app/utils/socket.tsx
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://board-game-server.vercel.app", {
-    transports: ['polling']  
-  });
+const socket = io("https://board-game-server.vercel.app");
+// http://localhost:
 export default socket;
