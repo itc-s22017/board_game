3a3903948a3494ec411dc610703e37a0c1fe9596
s22017 <itc-s22017@std.it-college.ac.jp>
2024-12-05 16:51:27 +0900

    vercelやめてrenderに変えてみた

--- app/utils/socket.tsx
+++ app/utils/socket.tsx
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://board-game-server.vercel.app");
+const socket = io("https://board-game-server-wouo.onrender.com");
 // http://localhost:
 export default socket;
