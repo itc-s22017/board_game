113d2519dfcbc1d5d3243f599581cf53c211396f
s22017 <itc-s22017@std.it-college.ac.jp>
2024-12-08 14:46:36 +0900

    urlみすってた

--- app/utils/socket.tsx
+++ app/utils/socket.tsx
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://board-game-server-wouo.onrender.com");
 // http://localhost:
 export default socket;
