2887ed5566605cab090a03f9933bd801c25aa8f6
s22017 <itc-s22017@std.it-college.ac.jp>
2024-12-12 18:31:30 +0900

    カンニング可能にした

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,6 +1,49 @@
+// import React from 'react';
+// import Image from 'next/image';
+// import tp from "../../public/img/tp.webp"
+
+// interface CardProps {
+//     img: string;
+//     isFlipped: boolean;
+//     onClick: () => void;
+// }
+
+// const Card: React.FC<CardProps> = ({ img, isFlipped, onClick }) => {
+//     const cardBackImage = tp.src;
+
+//     return (
+//         <div
+//             className="relative h-24 w-16 cursor-pointer [perspective:1000px]"
+//             onClick={onClick}
+//         >
+//             <div className={`absolute w-full h-full transition-transform duration-300 [transform-style:preserve-3d] ${isFlipped ? '[transform:rotateY(180deg)]' : ''}`}>
+//                 <div className="absolute w-full h-full [backface-visibility:hidden]">
+//                     <Image
+//                         src={cardBackImage}
+//                         alt="Card Back"
+//                         fill
+//                         className="rounded-lg object-cover"
+//                         sizes="64px"
+//                     />
+//                 </div>
+//                 <div className="absolute w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)]">
+//                     <Image
+//                         src={img}
+//                         alt="Card Front"
+//                         fill
+//                         className="rounded-lg object-cover"
+//                         sizes="64px"
+//                     />
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Card;
+
 import React from 'react';
 import Image from 'next/image';
-import tp from "../../public/img/tp.webp"
 
 interface CardProps {
     img: string;
@@ -9,30 +52,33 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ img, isFlipped, onClick }) => {
-    const cardBackImage = tp.src;
-
     return (
         <div
-            className="relative h-24 w-16 cursor-pointer [perspective:1000px]"
+            className={`relative h-24 w-16 cursor-pointer transition-transform duration-300 ease-in-out ${isFlipped ? 'rotate-y-180' : ''
+                }`}
             onClick={onClick}
         >
-            <div className={`absolute w-full h-full transition-transform duration-300 [transform-style:preserve-3d] ${isFlipped ? '[transform:rotateY(180deg)]' : ''}`}>
-                <div className="absolute w-full h-full [backface-visibility:hidden]">
-                    <Image
-                        src={cardBackImage}
-                        alt="Card Back"
-                        fill
-                        className="rounded-lg object-cover"
-                        sizes="64px"
-                    />
+            <div className="absolute h-full w-full rounded-lg shadow-md transition-all duration-300">
+                <div
+                    className={`absolute backface-hidden h-full w-full rounded-lg border-2 border-white bg-gradient-to-br from-blue-400 to-purple-500 ${isFlipped ? 'opacity-0' : 'opacity-100'
+                        }`}
+                >
+                    <div className="flex h-full items-center justify-center">
+                        <span className="text-2xl font-bold text-white">?</span>
+                    </div>
                 </div>
-                <div className="absolute w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)]">
+                <div
+                    className={`absolute backface-hidden h-full w-full overflow-hidden rounded-lg ${isFlipped ? 'opacity-100' : 'opacity-0'
+                        }`}
+                >
                     <Image
                         src={img}
-                        alt="Card Front"
+                        alt="Card"
                         fill
-                        className="rounded-lg object-cover"
-                        sizes="64px"
+                        priority
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
+                        style={{ objectFit: 'cover' }}
+                        className="rounded-lg"
                     />
                 </div>
             </div>
@@ -41,4 +87,5 @@ const Card: React.FC<CardProps> = ({ img, isFlipped, onClick }) => {
 };
 
 export default Card;
+
 
